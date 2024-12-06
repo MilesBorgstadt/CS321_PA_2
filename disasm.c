dbc6d5703f0632bd3388c2b142a79f1646ebc939
FreeLevistus <borgstadtmt@gmail.com>
2024-12-05 21:36:13 -0600

    stuff

--- disasm.c
+++ disasm.c
@@ -5,11 +5,13 @@ int main(int argc, char *argv[]) {
     FILE *file;
     file = fopen(argv[1], "rb");
 
-    int size = 0;
     char *output;
     int instruction;
 
     while (!feof(file)) {
-        
+        fread(&instruction, 4, 1, file);
+        instruction = be32toh(instruction);
+        output = realloc(output, sizeof(output) + 4);
+        getType(instruction);
     }
 }
